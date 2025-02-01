82b4b46b3660e93c8557e72f7a6042065e9bc653
flaycock <freddielaycock@protonmail.com>
2025-02-01 16:01:53 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+const App = () => (
+  <Box bg="blue.100" id="homepage">
+    <Heading size="xl">Clare Laycock Coaching</Heading>
+    <Heading size="md">Coming soon...</Heading>
+    <Button colorPalette="green">Click here to click a button</Button>
+  </Box>
+);
+
+export default App;
