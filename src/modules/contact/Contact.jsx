3162520f06f4d0381a3dd0799f261908c554311c
flaycock <freddielaycock@protonmail.com>
2025-04-05 19:00:40 +0100

    Optimising image file types and moving contact heading

--- src/modules/contact/Contact.jsx
+++ src/modules/contact/Contact.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
 
 import { CustomLink } from "../../components/custom-link";
 import { SectionContainer } from "../../components/section-container";
-import contact from "../../images/contact.jpg";
+import contact from "../../images/contact.webp";
+import { PRIMARY_FONT_COLOUR } from "../../constants";
 
 export const Contact = () => (
   <Box id="contact" paddingBottom="16px">
-    <SectionContainer bottom={true} heading="contact me">
+    <SectionContainer bottom={true}>
       <Box marginY="auto">
-        <Image
-          alt="Contact photo"
-          border="1px solid #9ac6b2"
-          borderRadius="12px"
-          htmlWidth="100%"
-          loading="lazy"
-          src={contact}
-        />
-      </Box>
-      <Box marginY="auto">
+        <Heading
+          as="h2"
+          color={PRIMARY_FONT_COLOUR}
+          paddingBottom="16px"
+          size={["3xl", "4xl", "4xl", "5xl", "6xl"]}
+          textAlign="center"
+          fontFamily="LeagueSpartan"
+          letterSpacing={["-3px", "-3px", "-4px", "-5px"]}
+        >
+          Contact me
+        </Heading>
         <Text textStyle={["md", "md", "xl", "2xl", "3xl"]}>
           Thanks for taking a look at my website! If you would like any more information, or would like to book
           a session, feel free to contact me via&nbsp;
@@ -33,6 +35,16 @@ export const Contact = () => (
           <br />
           Wish you all the best for whatever the future holds.
         </Text>
+      </Box>
+      <Box marginY="auto">
+        <Image
+          alt="Contact photo"
+          border="1px solid #9ac6b2"
+          borderRadius="12px"
+          htmlWidth="100%"
+          loading="lazy"
+          src={contact}
+        />
       </Box>
     </SectionContainer>
   </Box>
