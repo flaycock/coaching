feeab471f453d4f1f44bcc74a17fac75c7424a41
flaycock <freddielaycock@protonmail.com>
2025-04-18 15:59:37 +0100

    Making intro picture bigger and removing top padding on first testemonial

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -4,9 +4,10 @@ import { SECONDARY_FONT_COLOUR } from "../../constants";
 
 export const Testimonial = ({ quotes, sources }) => (
   <Box marginTop="16px" textAlign="center">
-    {quotes.map(quote => (
+    {quotes.map((quote, index) => (
       <Text
-        paddingY="8px"
+        paddingBottom="8px"
+        paddingTop={index === 0 ? "0" : "8px"}
         textStyle={["sm", "sm", "lg", "xl", "2xl"]}
       >
         {quote}
